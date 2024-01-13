5018ba7746537a22c0e5010bed084677ba78c63f
Harman <harmanmalova@gmail.com>
2024-01-13 09:26:42 +0300

    layout and shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 
 export default function Home() {
   return (
-    <div className='container mx-auto flex items-center justify-center min-h-screen tracking-tight text-teal-500 font-extrabold text-4xl'>
-      Welcome to the deepest dive into authentication with next auth
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className,
+        )}>
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton asChild>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   )
 }
