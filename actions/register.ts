440616b845533f51daa7b5649c887cf554a19125
Harman <harmanmalova@gmail.com>
2024-01-14 21:33:10 +0300

    prisma is super intuitive

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,37 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 
 import * as z from "zod";
 
-export const Register = async (values: z.infer<typeof LoginSchema>) => {
-  const ValidatedFields = LoginSchema.safeParse(values);
+export const Register = async (values: z.infer<typeof RegisterSchema>) => {
+  const ValidatedFields = RegisterSchema.safeParse(values);
   console.log(ValidatedFields);
   if (ValidatedFields.success === false) {
     return { error: "Invalid Fields" };
   }
 
-  return { success: "Email sent successfully" };
+  const { email, password, name } = ValidatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email already exists" };
+  }
+
+  const user = await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  //TODO: Send verification email to user
+  return { success: `${name} account created succesfully` };
 };
